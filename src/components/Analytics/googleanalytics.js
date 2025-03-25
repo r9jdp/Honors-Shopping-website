e561d16680b0a4f517f88c7600613d898bec46ec
rjdp <rajdeepvp273@gmail.com>
2025-03-25 16:47:50 +0530

    Added google analytics

--- src/components/Analytics/googleanalytics.js
+++ src/components/Analytics/googleanalytics.js
@@ -2,17 +2,22 @@ import Script from "next/script";
 
 export default function GoogleAnalytics() {
   return (
-    <Script
-      src="https://www.googletagmanager.com/gtag/js?id=G-SF997C7LEF"
-      strategy="afterInteractive"
-    >
-      <Script id="google-analytics" strategy="afterInteractive">
-        {`window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
+    <>
+      {/* Load Google Analytics script */}
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-SF997C7LEF"
+        strategy="afterInteractive"
+      />
 
-  gtag('config', 'G-SF997C7LEF');`}
+      {/* Initialize Google Analytics */}
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-SF997C7LEF');
+        `}
       </Script>
-    </Script>
+    </>
   );
 }
